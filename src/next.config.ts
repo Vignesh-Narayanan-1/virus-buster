f4d8b6bc4e86814abf202c5fd11cdd4b54e24a90
Vignesh Narayanan <vignarayanan04@gmail.com>
2025-07-15 15:19:43 +0000

    Now what? it says this

---
+++ src/next.config.ts
@@ -0,0 +1,32 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  // Add the following lines to enable static export for GitHub Pages
+  output: 'export',
+  // Replace 'your-repo-name' with the name of your GitHub repository
+  basePath: process.env.NODE_ENV === 'production' ? '/virus-buster' : '', 
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/virus-buster/' : '',
+
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // Un-optimize images for static export
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
